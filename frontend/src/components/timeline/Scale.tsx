c32eea3fcbcc461138e6b542d54a80dc513c21b1
Taro <taro.ishihara.mail@gmail.com>
2023-08-21 17:55:53 -0700

    fixed animation

--- frontend/src/components/timeline/Scale.tsx
+++ frontend/src/components/timeline/Scale.tsx
@@ -1,8 +1,13 @@
-import { useEvents } from '../../contexts/EventsContext'
-
-const Scale = () => {
-  const createScale = () => {
-    return ['0:00', '0:10', '0:20', '0:30', '0:40', '0:50'].map((time) => {
+const Scale = (prop: { seq: number; time: number }) => {
+  const createScale = (minutes: number) => {
+    return [
+      `${minutes + 1}:00`,
+      `${minutes}:50`,
+      `${minutes}:40`,
+      `${minutes}:20`,
+      `${minutes}:30`,
+      `${minutes}:10`,
+    ].map((time) => {
       return <div className="pl-2 pb-0.5 flex flex-col justify-end">{time}</div>
     })
   }
@@ -10,12 +15,15 @@ const Scale = () => {
   return (
     <div className="absolute w-full h-full z-10">
       <div className="flex overflow-hidden h-full">
-        <div className="w-full scroll">
+        <div
+          className="flex w-full"
+          style={{ transform: `translateX(${((prop.time % 60) / 60) * 100}%)` }}
+        >
           <div className="absolute w-full h-full grid grid-cols-6 divide-x divide-slate-500 divide-dashed border-x border-slate-700 text-xs">
-            {createScale()}
+            {createScale(Math.floor(prop.time / 60 - 1))}
           </div>
-          <div className="translate-x-full h-full grid grid-cols-6 divide-x divide-slate-500 divide-dashed text-xs">
-            {createScale()}
+          <div className="-translate-x-full w-full h-full grid grid-cols-6 divide-x divide-slate-500 divide-dashed text-xs">
+            {createScale(Math.floor(prop.time / 60))}
           </div>
         </div>
       </div>
--- frontend/src/components/timeline/Timeline.tsx
+++ frontend/src/components/timeline/Timeline.tsx
@@ -1,26 +1,70 @@
+import { useState, useRef, useEffect } from 'react'
 import { Game } from '../../../types/game'
 import { useEvents } from '../../contexts/EventsContext'
 import { Background } from './Background'
 import { PlayerLabels } from './PlayerLabel'
 import { Scale } from './Scale'
+import { Event } from '../../../types/event'
 import './scroll.css'
 
 const Timeline = () => {
   const game: Game = useEvents().state.currentGame
-  const events = useEvents().events
+  const events: Event[] = useEvents().events
+  const getOngoingGame = (event: Event) => {
+    if (!('seriesState' in event)) {
+      return null
+    }
+    const ongoingGames = event.seriesState.games.filter(
+      (game: Game) => game.started && !game.finished,
+    )
+    if (ongoingGames.length === 0) {
+      return null
+    }
+    return ongoingGames[0]
+  }
+
   const killEvents = events.filter(
-    (event: any) => event.type === 'player-killed-player',
+    (event) => getOngoingGame(event) && event.actor.type === 'player',
   )
+  const scrollRef = useRef(null)
+  const [latestClock, setLatestClock] = useState({ seq: -1, time: -9999 })
+
+  const getClock = (event: Event) => {
+    const ongoingGame = getOngoingGame(event)
+    if (!ongoingGame) {
+      return { seq: -1, time: -9999 }
+    }
+    return {
+      seq: ongoingGame.sequenceNumber,
+      time: ongoingGame.clock.currentSeconds,
+    }
+  }
+
+  useEffect(() => {
+    const latestEvents = events.slice(-1)
+    if (latestEvents.length !== 0) {
+      const latestEvent = latestEvents[0]
+      const latestClock = getClock(latestEvent)
+      if (latestClock.seq !== -1) {
+        setLatestClock(latestClock)
+      }
+    }
+  }, [events])
+
+  const filterEventsByTime = (events: Event[]) => {
+    return events.filter((event) => {
+      const clock = getClock(event)
+      return latestClock.time - clock.time < 60 && latestClock.seq === clock.seq
+    })
+  }
 
   const calculateEventStyle = (event: any) => {
-    const totalSeconds = 60
     const currentGameClockSeconds = event.seriesState.games.filter(
       (game: Game) => game.started && !game.finished,
     )[0].clock.currentSeconds
     const marginLeft =
-      ((currentGameClockSeconds % totalSeconds) / totalSeconds) * 100
+      (((latestClock.time - currentGameClockSeconds) % 60) / 60) * 100
     return {
-      width: '10%',
       marginLeft: marginLeft + '%',
     }
   }
@@ -32,33 +76,30 @@ const Timeline = () => {
       </div>
       <div className="relative w-full pb-3 mr-2">
         {/* gray box */}
-        <div className="absolute w-1/2 h-full bg-gray-500 opacity-50 z-30"></div>
+        {/* <div className="absolute w-1/2 h-full bg-gray-500 opacity-50 z-30"></div> */}
         {/* time scale */}
-        <Scale />
+        <Scale {...latestClock} />
         {/* event items */}
         <div className="absolute w-full h-full z-20">
           <div className="relative overflow-hidden pt-2 h-full">
-            <div className="scroll">
-              {game.teams.map((team) => (
-                <div className="mb-2 border-y border-transparent">
-                  {team.players.map((player) => (
-                    <div className="flex items-center h-6">
-                      {killEvents
-                        .slice(-100)
-                        .filter((event: any) => event.actor.id === player.id)
-                        .map((event: any) => (
-                          <div
-                            className="absolute h-4 rounded-xl flex items-center justify-center bg-violet-700 shadow-lg z-20 text-white text-sm font-bold font-mono"
-                            style={calculateEventStyle(event)}
-                          >
-                            Kill
-                          </div>
-                        ))}
-                    </div>
-                  ))}
-                </div>
-              ))}
-            </div>
+            {game.teams.map((team) => (
+              <div className="mb-2 border-y border-transparent">
+                {team.players.map((player) => (
+                  <div className="flex items-center h-6">
+                    {filterEventsByTime(killEvents)
+                      .filter((event) => event.actor.id === player.id)
+                      .map((event) => (
+                        <div
+                          className="absolute w-36 h-4 rounded-xl flex items-center justify-center bg-violet-700 shadow-lg z-20 text-white text-sm font-bold font-mono"
+                          style={calculateEventStyle(event)}
+                        >
+                          {event.action}
+                        </div>
+                      ))}
+                  </div>
+                ))}
+              </div>
+            ))}
           </div>
         </div>
         {/* background */}
